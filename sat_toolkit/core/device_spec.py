b74aef699504e869ccdad8e0e5c64ea4e63d6049
tkxb <wang3919379@163.com>
2024-12-16 15:14:15 +0800

    refactor: add deprecation warning for 'execute' method in DevicePluginSpec and improve documentation

--- sat_toolkit/core/device_spec.py
+++ sat_toolkit/core/device_spec.py
@@ -1,7 +1,12 @@
 import pluggy
+import warnings
 from sat_toolkit.models.Device_Model import Device
 
 hookspec = pluggy.HookspecMarker("device_mgr")
+
+"""
+Scan -> Init -> Connect -> Commands -> Close
+"""
 
 class DevicePluginSpec:
     @hookspec
@@ -17,12 +22,18 @@ class DevicePluginSpec:
         """Connect to the device."""
 
     @hookspec
-    def execute(self, device: Device, target: str):
-        """Execute an action on the target using the device."""
-
-    @hookspec
     def command(self, device: Device, command: str):
         """Send a command to the device."""
+
+    @hookspec
+    def execute(self, device: Device, target: str):
+        """Execute an action on the target using the device."""
+        warnings.warn(
+            "The 'execute' method is deprecated and will be removed in a future release. "
+            "Please use the 'command' method instead.",
+            DeprecationWarning,
+            stacklevel=2
+        )
 
     @hookspec
     def reset(self, device: Device):
