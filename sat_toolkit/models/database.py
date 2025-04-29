c491fd385b52ae4e7b91ce5e94d933cf8e77c8d3
tkxb <wang3919379@163.com>
2025-04-29 15:30:07 +0800

    feat: Update database configuration to use Django settings

--- sat_toolkit/models/database.py
+++ sat_toolkit/models/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from django.conf import settings
 
-db_path = os.path.join(os.path.dirname(__file__), 'iotsploit_database.sqlite')
+# Use Django's database file
+db_path = settings.DATABASES['default']['NAME']
 engine = create_engine(f'sqlite:///{db_path}', echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
